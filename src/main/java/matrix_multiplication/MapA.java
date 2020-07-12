077f652d8ab41cc60430b1245adc957de96f0b58
Arsh77 <arshabhsemwal9528@gmail.com>
2020-07-12 12:37:45 +0530

    Matrix Multiplication with 2 mappers

--- src/main/java/matrix_multiplication/MapA.java
+++ src/main/java/matrix_multiplication/MapA.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class Map extends Mapper<LongWritable, Text, Text, Text>{
+public class MapA extends Mapper<LongWritable, Text, Text, Text>{
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		
 		Configuration conf= context.getConfiguration();
@@ -17,20 +17,12 @@ public class Map extends Mapper<LongWritable, Text, Text, Text>{
 		String[] val = value.toString().split(" ");
 		Text outKey = new Text();
 		Text outVal = new Text();
-		if(val[0].equalsIgnoreCase("A")) {
-			for(int x=0; x<k; x++) {
-				outKey.set(val[1]+" "+x);
-				outVal.set(val[0]+" "+val[2]+" "+val[3]);
-				context.write(outKey, outVal);
+		if(val.length==4) {
+		for(int x=0; x<k; x++) {
+			outKey.set(val[1]+" "+x);
+			outVal.set(val[0]+" "+val[2]+" "+val[3]);
+			context.write(outKey, outVal);
 			}
-		}
-		else {
-			for(int x=0; x<i; x++) {
-				outKey.set(x+" "+val[2]);
-				outVal.set(val[0]+" "+val[1]+" "+val[3]);
-				context.write(outKey, outVal);
-			}
-		}
-				
+		}		
 	}
 }
--- src/main/java/matrix_multiplication/MatrixMultiplication.java
+++ src/main/java/matrix_multiplication/MatrixMultiplication.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -16,16 +17,15 @@ public class MatrixMultiplication {
 	
 	public static void main(String[] args) throws Exception {
 	
-		if (args.length != 5) {
+		if (args.length != 6) {
             System.err.println("Usage: MatrixMultiply <in_dir> <out_dir> <1st row> <common_row_col><2nd col>");
             System.exit(2);
         }
 		
 		Configuration conf = new Configuration();
-		System.out.println("@@## i : "+args[2]+" j : "+args[3]+" k : "+args[4]);
-		conf.set("i",args[2]);
-		conf.set("j",args[3]);
-		conf.set("k",args[4]);
+		conf.set("i",args[3]);
+		conf.set("j",args[4]);
+		conf.set("k",args[5]);
 		
 		Job job = new Job(conf, "MatrixMultiply");
 		
@@ -33,16 +33,13 @@ public class MatrixMultiplication {
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 		
-		System.out.println(">>>>>>>going in map<<<<<<<");
-		job.setMapperClass(Map.class);
-		System.out.println(">>>>>>>going in reduce<<<<<<<");
+		MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, MapA.class);
+		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, MapB.class);
 		job.setReducerClass(Reduce.class);
 		
-		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
-		
 		FileInputFormat.addInputPath(job, new Path(args[0]));
-	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[2]));
 		
 	    job.waitForCompletion(true);
 	    
